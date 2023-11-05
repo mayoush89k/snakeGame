70e6b2d5bc2b5b74abe905f2a9adeddb4ae1a51a
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-11-05 18:52:00 +0200

    snake game code

--- components/food.js
+++ components/food.js
@@ -1,5 +1,5 @@
-import { onSnake , expandSnake} from './snake.js'
-import { getRandomGridPosition } from './grid.js';
+import { onSnake , expandSnake} from '../snake.js'
+import { getRandomGridPosition } from '../grid.js';
 
 let food = getRandomFoodPosition();
 const expansionRate = 1 
